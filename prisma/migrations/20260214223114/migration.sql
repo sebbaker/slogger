6de9afed6677868420e00676d05dc0fdedde556e
Seb Baker <hey@sebbaker.com>
2026-02-15 13:54:16 +1100

    wip

--- prisma/migrations/20260214223114/migration.sql
+++ prisma/migrations/20260214223114/migration.sql
@@ -1,8 +1,14 @@
-/*
-  Warnings:
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-  - You are about to drop the column `search_text` on the `logs` table. All the data in the column will be lost.
+CREATE TABLE "logs" (
+  "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+  "source" TEXT NOT NULL,
+  "props" JSONB NOT NULL,
+  "time" TIMESTAMP(3) NOT NULL,
+  "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT "logs_pkey" PRIMARY KEY ("time", "id")
+) PARTITION BY RANGE ("time");
 
-*/
--- AlterTable
-ALTER TABLE "logs" DROP COLUMN "search_text";
+CREATE INDEX "logs_source_idx" ON "logs"("source");
+
+CREATE INDEX "logs_time_idx" ON "logs"("time");
